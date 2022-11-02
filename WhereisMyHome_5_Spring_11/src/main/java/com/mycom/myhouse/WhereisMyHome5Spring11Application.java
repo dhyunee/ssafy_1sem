18617ed9a22930a6817569b07b47f3d331543f2d
dhyunee <101089655+dhyunee@users.noreply.github.com>
2022-11-02 09:33:48 +0900

    spring pjt

---
+++ WhereisMyHome_5_Spring_11/src/main/java/com/mycom/myhouse/WhereisMyHome5Spring11Application.java
@@ -0,0 +1,13 @@
+package com.mycom.myhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereisMyHome5Spring11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereisMyHome5Spring11Application.class, args);
+	}
+
+}
---
+++ WhereisMyHome_5_Spring_11/src/test/java/com/mycom/myhouse/WhereisMyHome5Spring11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.myhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereisMyHome5Spring11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
